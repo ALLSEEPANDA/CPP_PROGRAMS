0ed94609abfdab01e1aec4ba8b96a55d58eab9fc
ALLSEEPANDA <allseepanda@gmail.com>
2024-11-10 13:32:38 +0530

    1

--- prg39.cpp
+++ prg39.cpp
@@ -2,26 +2,31 @@
 using namespace std;
 
 class Counter {
+private:
     static int count;
+
 public:
     Counter() {
         count++;
     }
+
     static void Display() {
-        cout << "Count: " << count << endl;
+        cout << "Current count: " << count << endl;
     }
 };
 
 int Counter::count = 0;
 
 int main() {
-    Counter c1;
     Counter::Display();
 
-    Counter c2;
+    Counter obj1;
     Counter::Display();
 
-    Counter c3;
+    Counter obj2;
+    Counter::Display();
+
+    Counter obj3;
     Counter::Display();
 
     return 0;
